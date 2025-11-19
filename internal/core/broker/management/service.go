62f338426ad4563d1cd43bdde4196c0c0728533d
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-11-18 21:09:52 -0300

    ✨ feat: implement ManagementService interface for broker management operations

---
+++ internal/core/broker/management/service.go
@@ -0,0 +1,64 @@
+package management
+
+import (
+	"github.com/andrelcunha/ottermq/internal/core/broker"
+	"github.com/andrelcunha/ottermq/internal/core/models"
+)
+
+// Service provides management operations for the broker.
+// This replaces the old ManagementAPI interface.
+
+type ManagementService interface {
+	// Queues
+	ListQueues(vhost string) ([]models.QueueDTO, error)
+	GetQueue(vhost, name string) (*models.QueueDTO, error)
+	CreateQueue(vhost string, req models.CreateQueueRequest) (*models.QueueDTO, error)
+	DeleteQueue(vhost, name string, ifUnused, ifEmpty bool) error
+	PurgeQueues(vhost, name string) (int, error)
+
+	// Exchanges
+	ListExchanges(vhost string) ([]models.ExchangeDTO, error)
+	GetExchange(vhost, name string) (*models.ExchangeDTO, error)
+	CreateExchange(vhost string, req models.CreateExchangeRequest) (*models.ExchangeDTO, error)
+	DeleteExchange(vhost, name string, ifUnused bool) error
+
+	// Bindings
+	ListBindings(vhost string) ([]models.BindingDTO, error)
+	ListQueuesBindings(vhost, queue string) ([]models.BindingDTO, error)
+	ListExchangeBindings(vhost, exchange string) ([]models.BindingDTO, error)
+	CreateBinding(vhost string, req models.CreateBindingRequest) (*models.BindingDTO, error)
+	DeleteBinding(vhost, exchangeName, queueName, routingKey string) error
+
+	// Consumers
+	ListConsumers(vhost string) ([]models.ConsumerDTO, error)
+	ListQueueConsumers(vhost, queue string) ([]models.ConsumerDTO, error)
+
+	// Connections
+	ListConnections() ([]models.ConnectionInfoDTO, error)
+	GetConnection(name string) (*models.ConnectionInfoDTO, error)
+	CloseConnection(name string, reason string) error
+
+	// Channels
+	ListChannels() ([]models.ChannelDTO, error)
+	ListConnectionChannels(connectionName string) ([]models.ChannelDTO, error)
+	GetChannel(connectionName string, channelNumber uint16) (*models.ChannelDTO, error)
+
+	// Messages
+	PublishMessage(vhost string, req models.PublishMessageRequest) error
+	GetMessages(vhost, queue string, count int, requeue, ackMode string) ([]string, error)
+
+	// VHosts
+	ListVHosts() ([]models.VHostDTO, error)
+	GetVHost(name string) (*models.VHostDTO, error)
+
+	// Overview/Stats
+	GetOverview() (*models.OverviewDTO, error)
+}
+
+type Service struct {
+	broker *broker.Broker
+}
+
+func NewService(b *broker.Broker) *Service {
+	return &Service{broker: b}
+}
