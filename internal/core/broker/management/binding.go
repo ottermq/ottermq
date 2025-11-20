9416c74f9bd4045d20d445061c9728f51f635dbb
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-11-20 17:14:33 -0300

    ♻️ refactor: implement ListQueuesBindings and enhance CreateBinding and DeleteBinding methods with error handling

--- internal/core/broker/management/binding.go
+++ internal/core/broker/management/binding.go
@@ -56,3 +56,77 @@ func listExchangeBindings(exchange *vhost.Exchange, vhostName string, bindingDto
 	}
 	return bindingDtos
 }
+
+func (s *Service) ListQueuesBindings(vhost, queueName string) ([]models.BindingDTO, error) {
+	vh := s.broker.GetVHost(vhost)
+	if vh == nil {
+		return nil, fmt.Errorf("vhost '%s' not found", vhost)
+	}
+
+	queue := vh.GetQueue(queueName)
+	if queue == nil {
+		return nil, fmt.Errorf("queue '%s' not found in vhost '%s'", queueName, vhost)
+	}
+
+	var bindingsList []models.BindingDTO
+
+	exchanges := vh.GetAllExchanges()
+	for _, exchange := range exchanges {
+		exchangeBindings := exchange.GetBindings()
+		for _, bindings := range exchangeBindings {
+			for _, binding := range bindings {
+				if binding.Queue.Name == queue.Name {
+					dto := models.BindingDTO{
+						VHost:       vhost,
+						Source:      exchange.NameOrAlias(),
+						Destination: binding.Queue.Name,
+						RoutingKey:  binding.RoutingKey,
+						Arguments:   binding.Args,
+					}
+					bindingsList = append(bindingsList, dto)
+				}
+			}
+		}
+	}
+
+	return bindingsList, nil
+}
+
+func (s *Service) CreateBinding(req models.CreateBindingRequest) (*models.BindingDTO, error) {
+	vh := s.broker.GetVHost(req.VHost)
+	if vh == nil {
+		return nil, fmt.Errorf("vhost '%s' not found", req.VHost)
+	}
+
+	exchangeName := req.Source
+	queue := req.Destination
+
+	err := vh.BindQueue(exchangeName, queue, req.RoutingKey, req.Arguments, nil)
+	if err != nil {
+		return nil, err
+	}
+	exchange := vh.GetExchange(exchangeName)
+	if exchange == nil {
+		return nil, fmt.Errorf("exchange '%s' not found in vhost '%s'", exchangeName, req.VHost)
+	}
+	dto := &models.BindingDTO{
+		VHost:       req.VHost,
+		Source:      exchange.NameOrAlias(),
+		Destination: queue,
+		RoutingKey:  req.RoutingKey,
+		Arguments:   req.Arguments,
+	}
+	return dto, nil
+}
+
+func (s *Service) DeleteBinding(req models.DeleteBindingRequest) error {
+	vh := s.broker.GetVHost(req.VHost)
+	if vh == nil {
+		return fmt.Errorf("vhost '%s' not found", req.VHost)
+	}
+
+	exchange := req.Source
+	queue := req.Destination
+
+	return vh.UnbindQueue(exchange, queue, req.RoutingKey, req.Arguments, nil)
+}
