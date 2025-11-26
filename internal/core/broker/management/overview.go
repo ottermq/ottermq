86368ec5d018145cd172e45fc9ba8601ad6e1f22
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-11-26 20:12:49 -0300

    ♻️ refactor: streamline GetMessageStats by consolidating queue data retrieval

--- internal/core/broker/management/overview.go
+++ internal/core/broker/management/overview.go
@@ -15,23 +15,21 @@ func (s *Service) GetOverview() (*models.OverviewDTO, error) {
 
 func (s *Service) GetMessageStats() models.OverviewMessageStats {
 	// acumulate message stats from all vhosts
-	vhosts := s.broker.ListVHosts()
 	var mStats models.OverviewMessageStats
 	qStats := []models.QueueMessageBreakdown{}
-	for _, vh := range vhosts {
-		dtos := s.getQueueDTOs(vh)
-		for _, dto := range dtos {
-			mStats.MessagesReady += dto.MessagesReady
-			mStats.MessagesUnacked += dto.MessagesUnacked
-			mStats.MessagesTotal += dto.MessagesTotal
-			qStats = append(qStats, models.QueueMessageBreakdown{
-				VHost:           vh.Name,
-				QueueName:       dto.Name,
-				MessagesReady:   dto.MessagesReady,
-				MessagesUnacked: dto.MessagesUnacked,
-			})
-		}
+	dtos := s.ListQueues()
+	for _, dto := range dtos {
+		mStats.MessagesReady += dto.MessagesReady
+		mStats.MessagesUnacked += dto.MessagesUnacked
+		mStats.MessagesTotal += dto.MessagesTotal
+		qStats = append(qStats, models.QueueMessageBreakdown{
+			VHost:           dto.VHost,
+			QueueName:       dto.Name,
+			MessagesReady:   dto.MessagesReady,
+			MessagesUnacked: dto.MessagesUnacked,
+		})
 	}
+
 	mStats.QueueStats = qStats
 	return mStats
 }
