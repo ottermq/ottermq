e5acbd23f72e7d6e5ae6f6f1d5bfd531646f2eab
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-11-26 20:12:50 -0300

    ♻️ refactor: update exchange methods to use explicit parameters for vhost and exchange name

--- internal/core/broker/management/exchange.go
+++ internal/core/broker/management/exchange.go
@@ -9,7 +9,7 @@ import (
 
 // ListExchanges lists all exchanges in the specified vhost.
 func (s *Service) ListExchanges() ([]models.ExchangeDTO, error) {
-	dtos := make([]models.ExchangeDTO, 0, 0)
+	dtos := make([]models.ExchangeDTO, 0)
 	for _, vh := range s.broker.ListVHosts() {
 		for _, exchange := range vh.GetAllExchanges() {
 			dto := models.ExchangeDTO{
@@ -24,15 +24,15 @@ func (s *Service) ListExchanges() ([]models.ExchangeDTO, error) {
 }
 
 // GetExchange retrieves details of a specific exchange in the specified vhost.
-func (s *Service) GetExchange(vhost, name string) (*models.ExchangeDTO, error) {
-	vh := s.broker.GetVHost(vhost)
+func (s *Service) GetExchange(vhostName, exchangeName string) (*models.ExchangeDTO, error) {
+	vh := s.broker.GetVHost(vhostName)
 	if vh == nil {
-		return nil, fmt.Errorf("vhost '%s' not found", vhost)
+		return nil, fmt.Errorf("vhost '%s' not found", vhostName)
 	}
 
-	exchange := vh.GetExchange(name)
+	exchange := vh.GetExchange(exchangeName)
 	if exchange == nil {
-		return nil, fmt.Errorf("exchange '%s' not found in vhost '%s'", name, vhost)
+		return nil, fmt.Errorf("exchange '%s' not found in vhost '%s'", exchangeName, vhostName)
 	}
 
 	dto := s.exchangeToDTO(vh, exchange)
@@ -41,10 +41,10 @@ func (s *Service) GetExchange(vhost, name string) (*models.ExchangeDTO, error) {
 }
 
 // CreateExchange creates a new exchange in the specified vhost.
-func (s *Service) CreateExchange(req models.CreateExchangeRequest) (*models.ExchangeDTO, error) {
-	vh := s.broker.GetVHost(req.VHost)
+func (s *Service) CreateExchange(vhostName, exchangeName string, req models.CreateExchangeRequest) (*models.ExchangeDTO, error) {
+	vh := s.broker.GetVHost(vhostName)
 	if vh == nil {
-		return nil, fmt.Errorf("vhost '%s' not found", req.VHost)
+		return nil, fmt.Errorf("vhost '%s' not found", vhostName)
 	}
 	exchangeType := vhost.ExchangeType(req.ExchangeType)
 	props := vhost.ExchangeProperties{
@@ -54,33 +54,33 @@ func (s *Service) CreateExchange(req models.CreateExchangeRequest) (*models.Exch
 		Internal:   req.Internal,
 		Arguments:  req.Arguments,
 	}
-	err := vh.CreateExchange(req.ExchangeName, exchangeType, &props)
+	err := vh.CreateExchange(exchangeName, exchangeType, &props)
 	if err != nil {
 		return nil, err
 	}
 
-	exchange := vh.GetExchange(req.ExchangeName)
+	exchange := vh.GetExchange(exchangeName)
 	dto := s.exchangeToDTO(vh, exchange)
 	return dto, nil
 }
 
 // DeleteExchange deletes an exchange from the specified vhost.
-func (s *Service) DeleteExchange(vhost, name string, ifUnused bool) error {
-	vh := s.broker.GetVHost(vhost)
+func (s *Service) DeleteExchange(vhostName, exchangeName string, ifUnused bool) error {
+	vh := s.broker.GetVHost(vhostName)
 	if vh == nil {
-		return fmt.Errorf("vhost '%s' not found", vhost)
+		return fmt.Errorf("vhost '%s' not found", vhostName)
 	}
-	exchange := vh.GetExchange(name)
+	exchange := vh.GetExchange(exchangeName)
 	if exchange == nil {
 		return nil // Idempotent delete
 	}
 	if ifUnused {
 		if exchange.BindingCount() > 0 {
-			return fmt.Errorf("exchange '%s' has active bindings", name)
+			return fmt.Errorf("exchange '%s' has active bindings", exchangeName)
 		}
 	}
 
-	return vh.DeleteExchange(name)
+	return vh.DeleteExchange(exchangeName)
 }
 
 func (*Service) exchangeToDTO(vh *vhost.VHost, exchange *vhost.Exchange) *models.ExchangeDTO {
