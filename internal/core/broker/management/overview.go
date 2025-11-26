ebbad342ba99bf1d0311e44c9c30e90f861b0c7c
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-11-26 20:33:27 -0300

    ♻️ refactor: implement GetOverview and GetMessageStats methods in Service

--- internal/core/broker/management/overview.go
+++ internal/core/broker/management/overview.go
@@ -3,5 +3,34 @@ package management
 import "github.com/andrelcunha/ottermq/internal/core/models"
 
 func (s *Service) GetOverview() (*models.OverviewDTO, error) {
-	panic("not implemented")
+	return &models.OverviewDTO{
+		BrokerDetails:   s.broker.GetBrokerOverviewDetails(),
+		NodeDetails:     s.broker.GetOverviewNodeDetails(),
+		ObjectTotals:    s.broker.GetObjectTotalsOverview(),
+		MessageStats:    s.GetMessageStats(),
+		ConnectionStats: s.broker.GetOverviewConnStats(),
+		Configuration:   s.broker.GetBrokerOverviewConfig(),
+	}, nil
+}
+
+func (s *Service) GetMessageStats() models.OverviewMessageStats {
+	// acumulate message stats from all vhosts
+	vhosts := s.broker.ListVHosts()
+	var mStats models.OverviewMessageStats
+	qStats := []models.QueueMessageBreakdown{}
+	for _, vh := range vhosts {
+		dtos := s.getQueueDTOs(vh)
+		for _, dto := range dtos {
+			mStats.MessagesReady += dto.MessagesReady
+			mStats.MessagesUnacked += dto.MessagesUnacked
+			mStats.MessagesTotal += dto.MessagesTotal
+			qStats = append(qStats, models.QueueMessageBreakdown{
+				VHost:           vh.Name,
+				QueueName:       dto.Name,
+				MessagesReady:   dto.MessagesReady,
+				MessagesUnacked: dto.MessagesUnacked,
+			})
+		}
+	}
+	return mStats
 }
