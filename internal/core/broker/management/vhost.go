fa28d4fd389de8dd0ffd0db02ad987675b95d47c
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-12-01 12:59:18 -0300

    ♻️ refactor: implement ListVHosts and GetVHost methods for vhost management

--- internal/core/broker/management/vhost.go
+++ internal/core/broker/management/vhost.go
@@ -3,9 +3,25 @@ package management
 import "github.com/andrelcunha/ottermq/internal/core/models"
 
 func (s *Service) ListVHosts() ([]models.VHostDTO, error) {
-	panic("not implemented")
+	vhosts := s.broker.ListVHosts()
+	response := make([]models.VHostDTO, 0, len(vhosts))
+	for _, vh := range vhosts {
+		dto := models.VHostDTO{
+			Name: vh.Name,
+		}
+		response = append(response, dto)
+	}
+	return response, nil
 }
 
 func (s *Service) GetVHost(name string) (*models.VHostDTO, error) {
-	panic("not implemented")
+	vh := s.broker.GetVHost(name)
+	if vh == nil {
+		return nil, nil
+	}
+	dto, err := s.broker.CreateVhostDto(vh)
+	if err != nil {
+		return nil, err
+	}
+	return &dto, nil
 }
