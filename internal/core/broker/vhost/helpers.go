df582da4819f415291ec519c44d6827a774bfd1d
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-12-04 14:31:47 -0300

    ✨ feat: implement parseMaxPriorityArgument function

--- internal/core/broker/vhost/helpers.go
+++ internal/core/broker/vhost/helpers.go
@@ -26,3 +26,20 @@ func convertToPositiveInt64(arg any) (int64, bool) {
 		return 0, false
 	}
 }
+
+func parseMaxPriorityArgument(args map[string]interface{}) (uint8, bool) {
+	maxPrio, ok := args["x-max-priority"]
+	if !ok {
+		return 0, false
+	}
+
+	value, ok := convertToPositiveInt64(maxPrio)
+	if !ok && value <= 0 {
+		return 0, false
+	}
+	if value > 255 {
+		value = 255
+	}
+
+	return uint8(value), true
+}
